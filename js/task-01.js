74bf96ca2c3ccc9832a5c502706bce324f498498
Wojtek-A <wojtek.a@hotmail.com>
2022-10-27 09:53:48 +0200

    Task-1

--- js/task-01.js
+++ js/task-01.js
@@ -1,22 +1,8 @@
 const categories = document.querySelectorAll(".item");
-
-const animals = categories[0];
-const animalsHeader = animals.querySelector("h2").textContent;
-const animalsElementsList = animals.querySelector("ul").children.length;
-
-const products = categories[1];
-const productsHeader = products.querySelector("h2").textContent;
-const productsElementsList = products.querySelector("ul").children.length;
-
-const technologies = categories[2];
-const technologiesHeader = technologies.querySelector("h2").textContent;
-const technologiesElementsList =
-  technologies.querySelector("ul").children.length;
-
 console.log("Number of categories:", categories.length);
-console.log("Category: ", animalsHeader);
-console.log("Elements: ", animalsElementsList);
-console.log("Category: ", productsHeader);
-console.log("Elements: ", productsElementsList);
-console.log("Category: ", technologiesHeader);
-console.log("Elements: ", technologiesElementsList);
+
+categories.forEach((item) => {
+  const header = item.querySelector("h2").textContent;
+  const elementList = item.querySelector("ul").children.length;
+  console.log(`${header}: ${elementList}`);
+});
