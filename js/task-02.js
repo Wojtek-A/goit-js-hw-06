8aa006e2e2dd91b6b9d50c2386d64b128465a9ce
Wojtek-A <wojtek.a@hotmail.com>
2022-10-27 10:30:16 +0200

    Task-2

--- js/task-02.js
+++ js/task-02.js
@@ -7,20 +7,9 @@ const ingredients = [
   "Condiments",
 ];
 const ingredientsList = document.querySelector("#ingredients");
-console.log(ingredientsList);
 
-const firstItem = document.createElement("li");
-firstItem.textContent = ingredients[0];
-const secondItem = document.createElement("li");
-secondItem.textContent = ingredients[1];
-const thirdItem = document.createElement("li");
-thirdItem.textContent = ingredients[2];
-const fourthIteam = document.createElement("li");
-fourthIteam.textContent = ingredients[3];
-const fifthIteam = document.createElement("li");
-fifthIteam.textContent = ingredients[4];
-const sixthIteam = document.createElement("li");
-sixthIteam.textContent = ingredients[5]
-
-ingredientsList.append(firstItem, secondItem, thirdItem, fourthIteam, fifthIteam, sixthIteam)
-
+ingredients.forEach((number, index) => {
+  const item = document.createElement("li");
+  item.textContent = ingredients[index];
+  ingredientsList.append(item);
+});
