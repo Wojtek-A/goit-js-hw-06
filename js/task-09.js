70936773703c8dccb2387d074f0ed76ca8a07a3f
Wojtek-A <wojtek.a@hotmail.com>
2022-10-31 17:35:13 +0100

    Task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const body = document.querySelector("body");
+const button = document.querySelector("button");
+const text = document.querySelector("span");
+
+button.addEventListener("click", () => {
+  const RandomHexColor = `#${Math.floor(Math.random() * 16777215).toString(
+    16
+  )}`;
+  body.style.backgroundColor = RandomHexColor;
+  text.textContent = `${RandomHexColor}`;
+});
