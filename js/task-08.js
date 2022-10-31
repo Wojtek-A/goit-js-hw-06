aa08bd142c5d770a78fa643e46db8283bb40288a
Wojtek-A <wojtek.a@hotmail.com>
2022-10-31 12:59:41 +0100

    Task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const inputEmail = document.querySelectorAll("input")[0];
+const inputPassword = document.querySelectorAll("input")[1];
+const form = document.querySelector(".login-form");
 
+inputEmail.addEventListener("blur", () => {
+  const inputLength = inputEmail.value.length;
+  if (inputLength === 0) {
+    alert("Wypełnij pole Email");
+  }
+});
+
+inputPassword.addEventListener("blur", () => {
+  const inputLength = inputPassword.value.length;
+  if (inputLength === 0) {
+    alert("Wypełnij pole Password");
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+});
